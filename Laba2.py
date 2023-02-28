5f0c9a7b04d895957dabc1c70f45b4264fec3490
Dina <107078656+jiriitsu@users.noreply.github.com>
2023-02-28 20:37:09 +0500

    Update Laba2.py

--- Laba2.py
+++ Laba2.py
@@ -12,3 +12,22 @@ def generate_twins(start, end):
 
 print("Числа-близнецы от 2 до 1000:")
 generate_twins(2, 1001)
+
+#2 способ
+def is_prime1(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+for i in range(2, 1001):
+    if is_prime1(i) and is_prime1(i + 2):
+        print("{:d} и {:d}".format(i, i+2))
+
+print()
+
+    
