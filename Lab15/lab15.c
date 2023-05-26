6834d1e14d843070775c6a1f92ce8b567f690caf
Dina <107078656+jiriitsu@users.noreply.github.com>
2023-05-26 23:09:57 +0500

    Update lab15.c

--- Lab15/lab15.c
+++ Lab15/lab15.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(int i) {
-    return sqrt(i+2) - sqrt(i) - (3/11);
+double f(double i) {
+    double drob;
+    drob = (double)3/11;
+    return sqrt(i+2) - sqrt(i) - drob;
 }
 
-double g(int j) {
+double g(double j) {
     return ((3*pow(j,2)) - 11) / (pow((-j), 3) - 2);
 }
 
@@ -14,21 +16,18 @@ int main() {
     double a[n][n];  
     double total_sum = 0;  
 
+    
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            a[i][j] = (f(i+1) + g(j+1));
+            a[i][j] = f(i+1) + g(i+1);
             total_sum += a[i][j];  
         }
     }
-    printf("%lf", g(10));
-    printf("\n");
-    printf("%lf\n", f(10));
     double total;
-    printf("\n");
-    double ch = 0.01;
-    printf("%lf\n", ch);
+    double ch;
+    ch = (double)1/100;
     total = (ch*total_sum);
-    printf("%lf\n", total_sum);
+
     printf("%lf\n", total);
    
     return 0;
